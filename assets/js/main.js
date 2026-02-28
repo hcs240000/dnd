a98ce6fb33a50c2edfa79f497eeaf58747fb4ea0
hcs240000 <hcs240000@utdallas.edu>
2026-02-28 04:35:12 +0000

    js listeners

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
 // main.js
 
 console.log("Hello, World!");
+
+let targetElement = document.querySelector("h2");
+
+console.log(targetElement.outerHTML);
+
+targetElement.addEventListener("mouseover", function() {
+    console.log(this.innerText);
+    this.innerText = "Crit!";
+    this.style = "cursor: wait";
+});
+
+targetElement.addEventListener("mouseout", function () {
+    this.innerText = "Fight";
+    this.style = "cursor: pointer";
+});
